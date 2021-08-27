b53d43df303544e87040789a235be4882f4279b3
syedmuhammadhussain <muhammadhussain@folio3.com>
2021-08-28 02:17:49 +0500

    day1

---
+++ values/values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+import "math/rand"
+
+func main() {
+	fmt.Println("go" + " lang")
+	fmt.Println("2+2 = ", 2+2)
+	fmt.Println(2+4, 2, 4/7, rand.Intn(680), rand.Intn(680), rand.Intn(680))
+	fmt.Println(true, !true, true)
+	fmt.Println(false, true)
+	fmt.Println(true && true)
+}
---
+++ variables/variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+
+	var b string = "initial"
+	var n int = 3
+	fmt.Println(b)
+	fmt.Println(a)
+	fmt.Println(n)
+	n = 45
+	fmt.Println(n)
+	name := "Hussain"
+	fmt.Println(name)
+	z := -45.4554532
+	mah_bool := int(z) > n
+	fmt.Println(mah_bool)
+
+}
